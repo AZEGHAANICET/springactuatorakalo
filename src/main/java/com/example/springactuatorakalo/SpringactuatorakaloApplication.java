53f1ca34a3c165dccd8e01f268953513c1cbba23
azegha <loicanicetazegha@gmail.com>
2024-01-05 22:40:58 +0100

    test

---
+++ src/main/java/com/example/springactuatorakalo/SpringactuatorakaloApplication.java
@@ -0,0 +1,13 @@
+package com.example.springactuatorakalo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringactuatorakaloApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringactuatorakaloApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springactuatorakalo/SpringactuatorakaloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springactuatorakalo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringactuatorakaloApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
